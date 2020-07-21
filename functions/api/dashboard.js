c1eb550b452e4d0e14cfbd6126922a655c0457e8
sdange <sdange@paypal.com>
2020-07-21 18:12:08 +0530

    Metadata and Dashboard code

--- functions/api/dashboard.js
+++ functions/api/dashboard.js
@@ -1,92 +1,97 @@
 const constants = require('../common/constants')
-const validate = require('../common/validator')
-const utils = require('../common/utils')
-const logger = require('../middleware/logger');
-const { getAllBranches } = require('./branch')
-const { getAllCategories } = require('./category')
-const { getAllProducts } = require('./product')
-const { getAllOperations } = require('./operation')
-const { getAllUnits } = require('./unit')
-const { getAllRoles } = require('./roles')
 const { getInventory } = require('./inventory')
-const joi = require('@hapi/joi');
 const admin = require('firebase-admin');
-const { isAdminOrSuperAdmin, isSuperAdmin } = require('../middleware/auth');
-const functions = require('firebase-functions')
-const audit = require('./audit')
 const express = require('express');
-const { extend } = require('@hapi/joi');
 const router = express.Router();
 const db = admin.firestore();
 
 /**
- * @description Route to retireve all metadata from firestore
+ * @description Route to retireve all data for the dashboard from firestore
+ * If the user is Admin or SuperAdmin return all dashboard data from all Branches
+ * If the user is Branch Userreturn dashboard data from that specific branch
  * @returns Json object containing all metadata
  */
-router.get("/", async (request, response, next) => { 
-    
-    var dashboardData = {}
-    const branchId = request.user.branch
-    if(request.user.role === constants.ADMIN || request.user.role === constants.SUPER_ADMIN) {
-        console.log(`Retrieving Dashboard data for ${request.user.role.toUpperCase()}`)
-        dashboardData = await getDashboardDataForAdminUser()
+router.get("/", async (request, response, next) => {
+
+    const userRole = request.user.role
+    console.debug(`Retrieving Dashboard data for ${userRole.toUpperCase()}`)
+    if (userRole === constants.ADMIN || userRole === constants.SUPER_ADMIN) {
+
+        //Get dashboard data for all branches
+        const dashboardDataForAllBranches = {}
+        const dashboardData = await getDashboardDataForAdminUser()
+        dashboardData.forEach(dashboard => {
+            const branchId = dashboard[constants.BRANCH]
+            delete dashboard[constants.BRANCH]
+            dashboardDataForAllBranches[branchId] = dashboard
+        })
+        console.debug('Retrieved dashboard data for all the branches')    
+        response.status(200).json(dashboardDataForAllBranches)
+
+    } else if (userRole === constants.BRANCH) {
+        
+        // Get dashboard data for branch 
+        const branchId = request.user.branch
+        console.log(`Retrieving Dashboard data for branch ${branchId}`)
+        const dashboardData = await getDashboardDataForBranchUser(branchId)
         response.status(200).json(dashboardData)
-    } else if (request.user.role === constants.BRANCH) {
-        console.log(`Retrieving Dashboard data for ${request.user.role.toUpperCase()} User`)
-        dashboardData = await getDashboardDataForBranchUser(branchId)
-    }  
+    }
+    
 })
 
+/**
+ * @description Method to generate dashboard data for admin users
+ * Internally calls getDashboardDataForBranchUser method for all branches
+ */
 async function getDashboardDataForAdminUser() {
-    var dashboardData = {}
+    
     const branchDocuments = await db.collection(constants.BRANCHES).listDocuments()
-    const dashboardPromises = []
-    for (const doc of branchDocuments) {
-        const p1 = getDashboardDataForBranchUser(doc.id)
-        dashboardPromises.push(p1)
-    }
-    var count = 1
-    Promise.all(dashboardPromises)
-        .then(branchDashboards => {
-            branchDashboards.forEach(branchDashboard => {
-                console.log(`COUNT ${count}`)
-                count = count + 1
-                const branchId = branchDashboard[constants.BRANCH]
-                delete branchDashboard[constants.BRANCH]
-                dashboardData[branchId] = branchDashboard 
-            })
-            return dashboardData
-        })
+    var dashboardPromises = []
+    branchDocuments.map(branch => {
+        const p1 = getDashboardDataForBranchUser(branch.id)
+        dashboardPromises.push(p1)  
+    })
+    return Promise.all(dashboardPromises)
 }
 
+/**
+ * @description Method to generate dashboard data for branch user
+ * Gets the data for just the users branch
+ * @param {*} branchId 
+ */
 async function getDashboardDataForBranchUser(branchId) {
     var dashboardData = {}
     dashboardData['branch'] = branchId
-    
-    const productsBelowThresholdPromise = getProductsUnderThreshold(branchId)
-    const recentTransactionsPromise = getRecentTransactions(branchId)
-    const pendingRequestsPromise = getPendingRequests(branchId)
-    
+    const branchRef = db.collection(constants.BRANCHES).doc(branchId);
+    const productsBelowThresholdPromise = getProductsUnderThreshold(branchRef)
+    const recentTransactionsPromise = getRecentTransactions(branchRef)
+    const pendingRequestsPromise = getPendingRequests(branchRef)
+
     dashboardData['productsBelowThreshold'] = await productsBelowThresholdPromise
     dashboardData['recentActivity'] = await recentTransactionsPromise
     dashboardData['pendingRequests'] = await pendingRequestsPromise
     return dashboardData
 }
 
-
-async function getProductsUnderThreshold(branchId) {
-    const branchRef = db.collection(constants.BRANCHES).doc(branchId);
-    const products = await getInventory(branchRef, branchId, true)
+/**
+ * Method to retrieve products under threshold for a given branch
+ * @param {*} branchId 
+ */
+async function getProductsUnderThreshold(branchRef) {
+    const products = await getInventory(branchRef, branchRef.id, true)
     return products.inventory
 }
 
-async function getRecentTransactions(branchId) {
-    const branchRef = db.collection(constants.BRANCHES).doc(branchId);
+/**
+ * Method to retrieve recent transactions for a given branch
+ * @param {*} branchRef 
+ */
+async function getRecentTransactions(branchRef) {
     let transactionSnapshot = await branchRef
-                        .collection(constants.TRANSACTIONS)
-                        .orderBy(constants.DATE, 'desc')
-                        .limit(10)
-                        .get()
+        .collection(constants.TRANSACTIONS)
+        .orderBy(constants.DATE, 'desc')
+        .limit(10)
+        .get()
 
     const transactions = []
     transactionSnapshot.forEach(transaction => {
@@ -97,12 +102,15 @@ async function getRecentTransactions(branchId) {
     return transactions
 }
 
-async function getPendingRequests(branchId) {
-    const branchRef = db.collection(constants.BRANCHES).doc(branchId);
+/**
+ * Method to retrieve pending requests for a given branch
+ * @param {} branchRef 
+ */
+async function getPendingRequests(branchRef) {
     let pendingRequestSnapshot = await branchRef
-                        .collection(constants.PENDING_REQUESTS)
-                        .orderBy(constants.DATE)
-                        .get()
+        .collection(constants.PENDING_REQUESTS)
+        .orderBy(constants.DATE)
+        .get()
 
     const pendingRequests = []
     pendingRequestSnapshot.forEach(pendingRequest => {
@@ -112,7 +120,5 @@ async function getPendingRequests(branchId) {
     })
     return pendingRequests
 }
-
-
 
 module.exports = router;
--- functions/api/metadata.js
+++ functions/api/metadata.js
@@ -1,30 +1,21 @@
 const constants = require('../common/constants')
-const validate = require('../common/validator')
-const utils = require('../common/utils')
-const logger = require('../middleware/logger');
 const { getAllBranches } = require('./branch')
 const { getAllCategories } = require('./category')
 const { getAllProducts } = require('./product')
 const { getAllOperations } = require('./operation')
 const { getAllUnits } = require('./unit')
 const { getAllRoles } = require('./roles')
-const joi = require('@hapi/joi');
-const admin = require('firebase-admin');
-const { isAdminOrSuperAdmin, isSuperAdmin } = require('../middleware/auth');
-const functions = require('firebase-functions')
-const audit = require('./audit')
 const express = require('express')
 const router = express.Router();
-const db = admin.firestore();
+
 
 /**
  * @description Route to retireve all metadata from firestore
  * @returns Json object containing all metadata
  */
 router.get("/", async (request, response) => {
-    const metadata = {
-        "metadata": []
-    }
+    console.info('Retrieving all metadata from firestore')
+    const metadataResponse = {}
     const metadataPromises = []
     
     const branchPromise = getAllBranches()
@@ -46,14 +37,35 @@ router.get("/", async (request, response) => {
     metadataPromises.push(unitPromise)
 
     Promise.all(metadataPromises)
-        .then(metadatas => {
-            metadatas.forEach(colMetadata => {
-                metadata[constants.METADATA].push(colMetadata)
+        .then(promiseArray => {
+            promiseArray.forEach(collectionData => {
+                addCollectionDataToResponse(metadataResponse, collectionData)
             })
             console.info('Returning all metadata to the client')
-            response.status(200).json(metadata)
+            response.status(200).json(metadataResponse)
         })
 
 });
+
+/**
+ * Utility function to create response object
+ * @param {*} metadata 
+ * @param {*} collectionData 
+ */
+function addCollectionDataToResponse(metadata, collectionData) {
+    if (collectionData.hasOwnProperty(constants.BRANCHES)) {
+        metadata[constants.BRANCHES] = collectionData[constants.BRANCHES]
+    } else if (collectionData.hasOwnProperty(constants.CATEGORIES)) {
+        metadata[constants.CATEGORIES] = collectionData[constants.CATEGORIES]
+    } else if (collectionData.hasOwnProperty(constants.PRODUCTS)) {
+        metadata[constants.PRODUCTS] = collectionData[constants.PRODUCTS]
+    } else if (collectionData.hasOwnProperty(constants.ROLES)) {
+        metadata[constants.ROLES] = collectionData[constants.ROLES]
+    } else if (collectionData.hasOwnProperty(constants.OPERATIONS)) {
+        metadata[constants.OPERATIONS] = collectionData[constants.OPERATIONS]
+    } else if (collectionData.hasOwnProperty(constants.UNITS)) {
+        metadata[constants.UNITS] = collectionData[constants.UNITS]
+    }
+}
 
 module.exports = router;
